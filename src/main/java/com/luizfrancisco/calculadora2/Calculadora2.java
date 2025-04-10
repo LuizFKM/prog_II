561035d206f53cd172535babca2cdedbf2a6a710
Luiz Francisco <luizfk.marinho@gmail.com>
2025-04-10 16:57:02 -0300

    calculadora com java swing e estrutura condicional

---
+++ src/main/java/com/luizfrancisco/calculadora2/Calculadora2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.luizfrancisco.calculadora2;
+
+import com.luizfrancisco.calculadora2.view.CalcView;
+
+/**
+ *
+ * @author User
+ */
+public class Calculadora2 {
+
+    public static void main(String[] args) {
+        CalcView calc = new CalcView();
+        calc.setVisible(true);
+        
+        
+    }
+}
